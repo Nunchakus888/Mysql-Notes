556a5d5db420f45707450b99b2729db496d3b3de
jiao.zhang <zhangjiaojiao2@rayootech.com>
2016-11-11 15:36:24 +0800

    (⊙o⊙)…

--- copyTable&insert&deleteABoundsData.sql
+++ copyTable&insert&deleteABoundsData.sql
@@ -4,4 +4,4 @@ CREATE TABLE IF NOT EXISTS t_log_history LIKE t_log #copy table but no data
 INSERT INTO t_log_backup4 VALUES(),(),(),(),() #INSERT bounds data
 
 delete from t_log_backup3 where 1=1 and LOGID IN ()
-delete from t_log_backup3 where 1=1 and LOGID = '123' OR LOGID = '232' OR LOGID = '323'
+delete from t_log_backup3 where 1=1 and LOGID = '123' OR LOGID = '232' OR LOGID = '323'   
